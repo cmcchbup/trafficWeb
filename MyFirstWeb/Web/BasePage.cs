4db588ec62f82c1ff1a80b69b9ae4624354e132a
cly441994447 <441994447@qq.com>
2015-01-10 20:29:44 +0800

    什么鬼？

---
+++ MyFirstWeb/Web/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NET.CLY
+{
+    public class BasePage:System.Web.UI.Page   //这个类是用来校验当前是否存在登陆的用户的
+    {
+        public Model.Login CurrentLoginUser { get; set; }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            CurrentLoginUser = Session["LoginUser"] as Model.Login;
+            if (CurrentLoginUser==null)
+            {   //如果不存在，就跳转到登陆界面
+                Response.Redirect("/Admin/Login.aspx");
+            }
+        }
+    }
+}
