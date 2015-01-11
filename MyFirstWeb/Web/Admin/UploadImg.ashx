641ad31c186cf692f3b87212fa5858c6ea7ea01b
cly441994447 <441994447@qq.com>
2015-01-11 19:31:22 +0800

    图片上传ok

---
+++ MyFirstWeb/Web/Admin/UploadImg.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="UploadImg.ashx.cs" Class="NET.CLY.Admin.UploadImg" %>
---
+++ MyFirstWeb/Web/Admin/UploadImg.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace NET.CLY.Admin
+{
+    /// <summary>
+    /// UploadImg 的摘要说明
+    /// </summary>
+    public class UploadImg : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            HttpPostedFile imgFile = context.Request.Files["img"];
+            string ext = Path.GetExtension(imgFile.FileName);
+            if (ext == ".jpg" || ext == ".png" || ext == ".jpeg")
+            {
+                string clientPath = "/UploadFiles/" + Guid.NewGuid().ToString() + ext;
+                string path = context.Request.MapPath(clientPath);
+                imgFile.SaveAs(path);
+                context.Response.Write(clientPath);
+            }
+            else
+            {
+                context.Response.Write("extError");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
