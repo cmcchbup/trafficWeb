57bf4ca390b8b5822fb38af5ed1ea73d4f0454c2
cly441994447 <441994447@qq.com>
2015-01-13 23:41:39 +0800

    大功告成！！！
    爽的飞起~

--- MyFirstWeb/Web/BasePageMini.cs
+++ MyFirstWeb/Web/BasePageMini.cs
@@ -5,21 +5,18 @@ using System.Web;
 
 namespace NET.CLY
 {
-    public class BasePage:System.Web.UI.Page   //这个类是用来校验当前是否存在登陆的用户的
+    public class BasePageMini:System.Web.UI.Page
     {
         public Model.Login CurrentLoginUser { get; set; }
 
         protected void Page_Init(object sender, EventArgs e)
         {
             CurrentLoginUser = Session["LoginUser"] as Model.Login;
-            if (CurrentLoginUser==null)
+            if (CurrentLoginUser == null)
             {   //如果不存在，就跳转到登陆界面
                 Response.Redirect("/Admin/Login.aspx");
             }
-            else if (CurrentLoginUser.UserType=="police")
-            {
-                Response.Redirect("/Admin/MainMini.aspx");
-            }
+            
         }
     }
 }
